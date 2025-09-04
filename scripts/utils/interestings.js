1879332ed4c4e807413c2d3997c9a92919306bf5
Programuzz <158294565+rikkudayo1@users.noreply.github.com>
2025-09-04 19:24:59 +0700

    Update interestings.js

--- scripts/utils/interestings.js
+++ scripts/utils/interestings.js
@@ -3,7 +3,7 @@ const datas = [
         title: "PAT 1 (2563)",
         description: "PAT 1 (ก.พ. 2563) มีเฉลยในไฟล์",
         subject_code: "TPAT1",
-        image_path: "../../images/Books/PAT.png",
+        image_path: "../images/Books/PAT.png",
         redirect_path: "https://rathcenter.com/old-web/Exam/Pat1/PAT16302.pdf",
     },
     {
@@ -119,4 +119,5 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     swiper.update();
+
 });
